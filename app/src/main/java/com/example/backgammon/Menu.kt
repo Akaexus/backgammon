ca3a63c5d350e3839eb675c62c1344529835c3f8
ms2jpg <28683657+ms2jpg@users.noreply.github.com>
2021-01-03 19:35:51 +0100

    layout główny

--- app/src/main/java/com/example/backgammon/Menu.kt
+++ app/src/main/java/com/example/backgammon/Menu.kt
@@ -1,20 +1,25 @@
 package com.example.backgammon
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class Menu : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         var user: User? = intent.getParcelableExtra<User>("user")
 
-        if (user != null) {
-            Log.i("backgammon_debug", user.uid.toString())
-            Toast.makeText(this, user.login, Toast.LENGTH_LONG).show()
+        var btn_quit = requireViewById<Button>(R.id.btn_quit)
+        btn_quit.setOnClickListener {
+            finishAffinity()
         }
+
     }
 }
